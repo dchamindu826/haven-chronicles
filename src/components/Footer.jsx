81fc51ce974d6e523d3700a186233831758ffce5
dchamindu826 <dchamindu826@gmail.com>
2025-07-16 21:34:52 +0530

    footers

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 // src/components/Footer.jsx
 import React from 'react';
-import { FaBeer, FaFacebook, FaTwitter } from "react-icons/fa";
+import { FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
 import './Footer.css';
 
 function Footer() {
